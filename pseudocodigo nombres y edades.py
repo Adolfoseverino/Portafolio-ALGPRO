7e5289e3e208846516946ecc3c1ebf698c64d322
Adolfoseverino <50465726+Adolfoseverino@users.noreply.github.com>
2019-05-14 16:38:48 -0400

    Create pseudocodigo nombres y edades.py

---
+++ pseudocodigo nombres y edades.py
@@ -0,0 +1,6 @@
+print ("digite su nombre")
+nombre = input()
+print ("digite su edad:")
+edad = int(input())
+
+print ("Hola " + nombre + "usted tiene" + edad + "anos y en diez anos tendra" + (edad + 10)
