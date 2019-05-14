f82cd9427e219e5944b9a29cc67fa944ba287145
Adolfoseverino <50465726+Adolfoseverino@users.noreply.github.com>
2019-05-14 16:56:52 -0400

    cambio
    str

---
+++ pseudocodigo nombre y edades.py
@@ -0,0 +1,5 @@
+print ("digite su nombre:")
+nombre = input()
+print ("digite su edad:")
+edad = int(input())
+print ("Hola ," + nombre + ",usted tiene" + str(edad) + "anos, y en diez anos tendra" + str(edad + 10))
--- pseudocodigo nombres y edades.py
+++
@@ -1,6 +0,0 @@
-print ("digite su nombre")
-nombre = input()
-print ("digite su edad:")
-edad = int(input())
-
-print ("Hola " + nombre + "usted tiene" + str(edad) + "anos y en diez anos tendra" + str(edad + 10)
